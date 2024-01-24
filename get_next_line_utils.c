a136f62ca857e8f4b4aa476b28cfd500785a8002
FinestRyuk <40182073+ajilidali@users.noreply.github.com>
2024-01-24 14:12:35 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -54,8 +54,6 @@ char	*ft_strjoin(char *s1, char *s2)
 		if (s1)
 			s1[0] = '\0';
 	}
-	if (!s1 || !s2)
-		return (NULL);
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
